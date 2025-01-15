e90a53dbd2eadcc296d7b2fa207294d1af735648
atikfaisalemon <atikfaisalemon@gmail.com>
2025-01-15 16:12:36 +0600

    Updated Header 3xl

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="text-2xl font-semibold text-left">TapaScript Cars</div>
+    <div className="text-3xl font-semibold text-left">TapaScript Cars</div>
   );
 };
 export default Header;
